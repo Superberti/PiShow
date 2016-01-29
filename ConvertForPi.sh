6453f06f4bf710f2e26b4cf5ad58ceea6068e315
Superberti <orutsch@mailbox.org>
2016-01-29 08:53:44 +0100

    Convert script enhanced

--- ConvertForPi.sh
+++ ConvertForPi.sh
@@ -24,23 +24,38 @@ errMsg()
 
 
 # get infile and outfile
-infile="$1"
-outfile="$2"
+#infile="$1"
+#outfile="$2"
 
 
 # test that infile provided
-[ "$infile" = "" ] && errMsg "NO INPUT FILE SPECIFIED"
+#[ "$infile" = "" ] && errMsg "NO INPUT FILE SPECIFIED"
 
 # test that outfile provided
-[ "$outfile" = "" ] && errMsg "NO OUTPUT FILE SPECIFIED"
+#[ "$outfile" = "" ] && errMsg "NO OUTPUT FILE SPECIFIED"
 
+CONV_DIRNAME="conv"
+for InputFile in $*
+do 
+  outfile=$(basename "$InputFile")
+  EXT="${outfile##*.}"
+#  echo EXT:$EXT
+  OUT_WO_EXT="${outfile%.*}"
+#  echo OUT_WO_EXT:$OUT_WO_EXT
+  DIR=$(dirname "${InputFile}")
+  if [ ! -d "${DIR}/${CONV_DIRNAME}" ]; then
+    mkdir "${DIR}/${CONV_DIRNAME}"
+  fi
+  outfile=${DIR}/${CONV_DIRNAME}/conv_${OUT_WO_EXT}.${EXT}
+  echo Bearbeite $InputFile nach $outfile
+  test=`identify -format '%[exif:orientation]' $InputFile`
+  echo Format:$test
+  if [ $test -eq 1 ]; then
+    convert $InputFile -auto-orient -ordered-dither o8x8,64,64,64 -quality 97 -resize 1920x $outfile
+  else
+    convert $InputFile -auto-orient -ordered-dither o8x8,64,64,64 -quality 97 -resize x1200 $outfile
+  fi
+done
 
-test=`identify -format '%[exif:orientation]' $infile`
-echo $test
-if [ $test -eq 1 ]; then
-  convert $infile -auto-orient -ordered-dither o8x8,64,64,64 -quality 97 -resize 1920x $outfile
-else
-  convert $infile -auto-orient -ordered-dither o8x8,64,64,64 -quality 97 -resize x1200 $outfile
-fi
 
 exit 0
