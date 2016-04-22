93796d5c8348dfc3e50c29b7e20e04f8e53e3a1b
Superberti <orutsch@mailbox.org>
2016-04-22 16:23:26 +0200

    Added test for existing images

--- ConvertImages.py
+++ ConvertImages.py
@@ -1,59 +1,89 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Hier kommen die unbearbeiteten Bilder an
 rootdir="/samba/public/"
 convdir="~/Pictures/conv/"
-#print ("Hallo, Welt!üü")
+tempdir="/tmp/"
+
 import subprocess
 import os
-
-#MyOutput = subprocess.check_output('ls -l '+rootdir, shell=True, stderr=subprocess.STDOUT)
-#for line in p.stdout.readlines():
-#    print (line),
-#retval = p.wait()
-#print(MyOutput)
-
 import glob
-ImageList = glob.glob(rootdir+"*.JPG")
-ImageList2=glob.glob(rootdir+"*.jpg")
-ImageList.extend(ImageList2)
+import time
+import filecmp
+import shutil
 
-#print (ImageList)
+print ("Raspi-Bilderrahmen-Konverter gestartet...")
 
-for ImageFileName in ImageList:
-    #print("Bearbeite Bild: "+ImageFileName)
+while 1==1:
+    ImageList = glob.glob(rootdir+"*.JPG")
+    ImageList2=glob.glob(rootdir+"*.jpg")
+    ImageList.extend(ImageList2)
 
-    ExifArgsString=subprocess.check_output('exiftool -s -s -s -n -ImageWidth -ImageHeight -Orientation -ImageDescription '+ImageFileName, shell=True, stderr=subprocess.STDOUT)
-    #print (ExifArgs)
-
-    ExifArgs=ExifArgsString.decode("utf-8").splitlines()
-    #print(ExifArgs)
-
-    Width=int(ExifArgs[0])
-    Height=int(ExifArgs[1])
-    Orientation=int(ExifArgs[2])
-    Description=ExifArgs[3]
-
-    BaseFileName=os.path.splitext(os.path.basename(ImageFileName))[0]
-    #print (BaseFileName)
-    #print("w:"+repr(Width)+" h:"+repr(Height)+" o:"+repr(Orientation))
-    
-    NewFileName=os.path.join(convdir,BaseFileName+"_conv.JPG")
-    print ("Konvertiere<"+ImageFileName+"> nach <"+NewFileName+">...", end="")
-
-    if Orientation==1 and Width>=Height:
-        print("querformat...",end="")
-        ResizeVal="-resize 1920x"
+    if len(ImageList)>0:
+        print("Neue Bilder erkannt. Starte Konversion...")
     else:
-        print("hochformat...",end="")
-        ResizeVal="-resize x1200"
+        print("Nächste Überprüfung in 5 Minuten.")
+        time.sleep(30)
+
+    # Bilder konvertieren
+    for ImageFileName in ImageList:
+        #print("Bearbeite Bild: "+ImageFileName)
+
+        ExifArgsString=subprocess.check_output('exiftool -s -s -s -n -ImageWidth -ImageHeight -Orientation -ImageDescription '+ImageFileName, shell=True, stderr=subprocess.STDOUT)
+        #print (ExifArgs)
+
+        ExifArgs=ExifArgsString.decode("utf-8").splitlines()
+        #print(ExifArgs)
+
+        Width=int(ExifArgs[0])
+        Height=int(ExifArgs[1])
+        Orientation=int(ExifArgs[2])
+        Description=ExifArgs[3]
+
+        BaseFileName=os.path.splitext(os.path.basename(ImageFileName))[0]
+        #print (BaseFileName)
+        #print("w:"+repr(Width)+" h:"+repr(Height)+" o:"+repr(Orientation))
         
-    try:
-        grepOut=subprocess.check_output("convert "+ImageFileName+" -auto-orient -ordered-dither o8x8,64,64,64 -quality 97 "+ResizeVal+" "+NewFileName, shell=True)
-    except subprocess.CalledProcessError as repexc:
-        print ("convert failed with code", grepexc.returncode, grepexc.output )
+        NewFileName=os.path.join(convdir,BaseFileName+"_conv.JPG")
+        print ("Konvertiere<"+ImageFileName+"> nach <"+NewFileName+">...", end="")
 
-    print("fertig.")
+        if Orientation==1 and Width>=Height:
+            print("querformat...",end="")
+            ResizeVal="-resize 1920x"
+        else:
+            print("hochformat...",end="")
+            ResizeVal="-resize x1200"
+            
+        try:
+            OrigNewFileName=NewFileName
+            RenameFile=False
+            if os.path.isfile(NewFileName):
+                NewFileName=os.path.join(tempdir,BaseFileName+"_conv.JPG")
+                RenameFile=True
+                
+            grepOut=subprocess.check_output("convert "+ImageFileName+" -auto-orient -ordered-dither o8x8,64,64,64 -quality 97 "+ResizeVal+" "+NewFileName, shell=True)
+            
+            if RenameFile:
+                if filecmp(NewFileName,OrigNewFileName):
+                    print ("Das Bild <"+OrigNewFileName+"> exisitert bereits. Keine Aktion notwendig!")
+                else:
+                    print ("Der Bildname <"+OrigNewFileName+"> exisitert zwar bereits, aber der Bildinhalt ist unterschiedlich. Benenne Zieldatei um!")
+                    ImageCounter=1
+                    NewFileName=os.path.join(convdir,BaseFileName+"_"+str(ImageCounter)+"_conv.JPG")
+                    while os.path.isfile(NewFileName):
+                        ImageCounter+=1
+                        NewFileName=os.path.join(convdir,BaseFileName+"_"+str(ImageCounter)+"_conv.JPG")
 
+                    TempFileName=os.path.join(tempdir,BaseFileName+"_conv.JPG")
+                    shutil.copy2(TempFileName, NewFileName)
+                    os.remove(TempFileName)
+                    
+            os.remove(ImageFileName)
+        except subprocess.CalledProcessError as repexc:
+            print ("convert Fehlercode", grepexc.returncode, grepexc.output )
+
+        print("fertig.")
+
+    
     
